864482c5549f007f116c587847f6425fd0bccc7f
mahmoud <mahmoudbouchikhi@gmail.com>
2015-10-24 06:25:01 -0600

    word_jumble_game, will need to add some more rules to it.

--- Desktop/Python Scripts Ubu/word_jumble.py
+++ Desktop/Python Scripts Ubu/word_jumble.py
@@ -6,11 +6,18 @@
 import random
 
 # create a sequence of words to choose from
+from gi.overrides.keysyms import j
+
 WORDS = ("python", "jumble", "easy", "difficult", "answer", "xylophone")
+vocabulary = ("program", "goggle", "not difficult", "after a question", "phone")
+hint=""
 # pick one word randomly from the sequence
 word = random.choice(WORDS)
 # create a variable to use later to see if the guess is correct
 correct = word
+[WORDS]=[vocabulary]
+
+
 
 # create a jumbled version of the word
 jumble =""
@@ -31,13 +38,24 @@ print(
 print("The jumble is:", jumble)
 
 guess = input("\nYour guess: ")
-while guess != correct and guess != "":
-    print("Sorry, that's not it.")
-    guess = input("Your guess: ")
-    
-if guess == correct:
-    print("That's it!  You guessed it!\n")
+while guess != "hint":
+    if guess != correct:
+        print("Sorry, that's not it. and remember that you can get help at anytime by typing hint")
+        guess = input("Your guess: ")
 
-print("Thanks for playing.")
+    elif guess == correct:
+        print("That's it!  You guessed it!\n")
+        print("Thanks for playing.")
+else :
+
+        i=j
+        print(j)
+
 
 input("\n\nPress the enter key to exit.")
+###
+def hint(WORDS, vocabulary):
+    count = 0
+
+#    for i in WORDS:
+#        return vocabulary
